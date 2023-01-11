5f53c019942eae9b07b3d21837d1ee6f61d0956d
Yash Chhatre <ychhatre@gmail.com>
2023-01-10 23:37:16 -0800

    finished all swervecode, waiting on real time testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,14 +4,20 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -24,6 +30,11 @@ public final class Constants {
         public static final double LENGTH = Units.inchesToMeters(0);
 
         public static final double WHEEL_CIRCUMFERENCE = Units.inchesToMeters(4) * Math.PI;
+        public static final SwerveDriveKinematics SWERVE_DRIVE_KINEMATICS = new SwerveDriveKinematics(
+                new Translation2d(-RobotDimensions.LENGTH / 2, RobotDimensions.WIDTH / 2),
+                new Translation2d(-RobotDimensions.LENGTH / 2, -RobotDimensions.WIDTH / 2),
+                new Translation2d(RobotDimensions.LENGTH / 2, RobotDimensions.WIDTH / 2),
+                new Translation2d(RobotDimensions.LENGTH / 2, -RobotDimensions.WIDTH / 2));
     }
 
     public static final class Drive {
@@ -33,6 +44,16 @@ public final class Constants {
     }
 
     public static final class Motor {
-        // Motor indexes here
+        public static final int SWERVE_FRONT_LEFT_POWER = 4;
+        public static final int SWERVE_FRONT_LEFT_STEER = 15;
+
+        public static final int SWERVE_FRONT_RIGHT_POWER = 3;
+        public static final int SWERVE_FRONT_RIGHT_STEER = 14;
+
+        public static final int SWERVE_BACK_LEFT_POWER = 2;
+        public static final int SWERVE_BACK_LEFT_STEER = 13;
+
+        public static final int SWERVE_BACK_RIGHT_POWER = 1;
+        public static final int SWERVE_BACK_RIGHT_STEER = 12;
     }
 }
