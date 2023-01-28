ba665fcde4e83c65d86062b2ff1c82a14ebd6e49
Jeff Spragg <jwspragg@mtu.edu>
2023-01-28 03:40:46 -0800

    Clean up arm code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -15,15 +15,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.sorutil.SorMath;
 import frc.sorutil.motor.MotorConfiguration;
-import frc.sorutil.motor.PidProfile;
 import frc.sorutil.motor.SensorConfiguration;
-import frc.sorutil.motor.SuController;
 import frc.sorutil.motor.SuSparkMax;
 import frc.sorutil.motor.SuTalonFx;
 import frc.sorutil.motor.SuController.ControlMode;
 
 public class ArmSubsystem extends SubsystemBase {
   private final Logger logger;
+
   private SuTalonFx jointA;
   private SuTalonFx jointB;
   private SuSparkMax cascade;
@@ -32,23 +31,29 @@ public class ArmSubsystem extends SubsystemBase {
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
     logger = Logger.getLogger(ArmSubsystem.class.getName());
+
     MotorConfiguration jointMotorConfig = new MotorConfiguration();
-    MotorConfiguration cascadeMotorConfig = new MotorConfiguration();
-    jointMotorConfig.setPidProfile(new PidProfile(0, 0, 0));// placeholders
-    jointMotorConfig.setCurrentLimit(Constants.Motor.ARM_JOINT_CURRENT_LIMIT);
-    cascadeMotorConfig.setPidProfile(new PidProfile(0, 0, 0));// placeholders
-    cascadeMotorConfig.setCurrentLimit(Constants.Motor.ARM_CASCADE_CURRENT_LIMIT);
+
+    jointMotorConfig.setPidProfile(Constants.Arm.ARM_PID_PROFILE);
+    jointMotorConfig.setCurrentLimit(Constants.Arm.ARM_JOINT_CURRENT_LIMIT);
     SensorConfiguration jointSensorConfiguration = new SensorConfiguration(
-        new SensorConfiguration.IntegratedSensorSource(1));
-    SensorConfiguration cascadeSensorConfiguration = new SensorConfiguration(
         new SensorConfiguration.IntegratedSensorSource(1));
     jointA = new SuTalonFx(new WPI_TalonFX(Constants.Motor.ARM_JOINT_INDEX), "Joint Motor A", jointMotorConfig,
         jointSensorConfiguration);
     jointB = new SuTalonFx(new WPI_TalonFX(Constants.Motor.ARM_JOINT_FOLLOWER_INDEX), "Joint Motor B", jointMotorConfig,
         jointSensorConfiguration);
+    jointB.follow(jointA);
+
+    MotorConfiguration cascadeMotorConfig = new MotorConfiguration();
+    cascadeMotorConfig.setPidProfile(Constants.Arm.CASCADE_PID_PROFILE);
+    cascadeMotorConfig.setCurrentLimit(Constants.Arm.ARM_CASCADE_CURRENT_LIMIT);
+    SensorConfiguration cascadeSensorConfiguration = new SensorConfiguration(
+        new SensorConfiguration.IntegratedSensorSource(1));
+
     cascade = new SuSparkMax(new CANSparkMax(Constants.Motor.ARM_CASCADE_INDEX, MotorType.kBrushless), "Cascade Motor",
         cascadeMotorConfig, cascadeSensorConfiguration);
-    jointB.follow(jointA);
+
+    logger.info("Arm Initialized.");
   }
 
   /*
@@ -60,6 +65,7 @@ public class ArmSubsystem extends SubsystemBase {
     intendedPosition = pair;
   }
 
+  // TODO: Fix this, callers should not be expected to convert from polar coordinates.
   public Pair<Double, Double> getManipulatorPosition() {
     double r = 0.0444754 / 2;
     double theta = Math.toRadians(jointA.outputPosition());
@@ -69,7 +75,9 @@ public class ArmSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run4
+    // This method will be called once per scheduler run
+    // TODO: This should be changed to run a constant program that seeks the correct
+    // position while keeping the arm safe.
     double[] vec = SorMath.cartesianToPolar(intendedPosition.getFirst(), intendedPosition.getSecond());
     jointA.set(ControlMode.POSITION, Math.toDegrees(vec[1]));
     cascade.set(ControlMode.POSITION, vec[0]);
