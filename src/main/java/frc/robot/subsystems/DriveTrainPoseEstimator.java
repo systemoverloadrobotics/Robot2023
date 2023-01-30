0a0c66453ecbf10641b64b7e857479b257af7e75
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2023-01-30 09:22:13 -0800

    method to addvision measurement

--- src/main/java/frc/robot/subsystems/DriveTrainPoseEstimator.java
+++ src/main/java/frc/robot/subsystems/DriveTrainPoseEstimator.java
@@ -4,12 +4,9 @@
 
 package frc.robot.subsystems;
 
-import java.util.logging.Logger;
-
-import org.photonvision.PhotonCamera;
-
 import edu.wpi.first.math.MatBuilder;
 import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -18,23 +15,38 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import java.util.logging.Logger;
+import org.photonvision.PhotonCamera;
 
 public class DriveTrainPoseEstimator extends SubsystemBase {
+
   private final Logger logger;
   private Swerve swerve;
   private Vision vision;
   private SwerveDrivePoseEstimator poseEstimator;
- 
 
   public DriveTrainPoseEstimator() {
-   
     logger = Logger.getLogger(DriveTrainPoseEstimator.class.getName());
-    poseEstimator = new SwerveDrivePoseEstimator(Constants.RobotDimensions.SWERVE_DRIVE_KINEMATICS,
-        swerve.getHeading(), swerve.getModulePositions(), new Pose2d(),
+    poseEstimator =
+      new SwerveDrivePoseEstimator(
+        Constants.RobotDimensions.SWERVE_DRIVE_KINEMATICS,
+        swerve.getHeading(),
+        swerve.getModulePositions(),
+        new Pose2d(),
         new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.1, 0.1, 0.1),
-        new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.1, 0.1, 0.1));
-    var visionEstimatedPose = vision.getEstimatedGlobalPose(getEstimatedPose());
-    poseEstimator.addVisionMeasurement(visionEstimatedPose.getFirst(), Timer.getFPGATimestamp());
+        new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.1, 0.1, 0.1)
+      );
+  }
+
+  public void addVisionMeasurement(
+    Pose2d visionMeasurement,
+    double timestampSeconds
+  ) {
+    poseEstimator.addVisionMeasurement(
+      visionMeasurement,
+      timestampSeconds,
+      VecBuilder.fill(0, 0, 0) //TODO: Add vision measurement covariance
+    );
   }
 
   public Pose2d getEstimatedPose() {
@@ -43,8 +55,11 @@ public class DriveTrainPoseEstimator extends SubsystemBase {
 
   @Override
   public void periodic() {
-    poseEstimator.updateWithTime(Timer.getFPGATimestamp(), swerve.getHeading(), swerve.getModulePositions());
-   
+    poseEstimator.updateWithTime(
+      Timer.getFPGATimestamp(),
+      swerve.getHeading(),
+      swerve.getModulePositions()
+    );
   }
 
   @Override
