328af9f2926b1bcaebd2eee3833e57caab775c26
_Kota_ <94937056+Siddarth-Kota@users.noreply.github.com>
2023-02-05 18:34:19 -0800

    Finished MoveToGrid Method and Started MoveToScoringLocation and Created GridSelector helper class

--- src/main/java/frc/robot/commands/MoveToScoringLocation.java
+++ src/main/java/frc/robot/commands/MoveToScoringLocation.java
@@ -5,56 +5,48 @@
 package frc.robot.commands;
 
 import frc.robot.subsystems.Swerve;
-import frc.robot.Constants;
+import frc.robot.GridSelector.GridLocation;
 import frc.robot.subsystems.DriveTrainPoseEstimator;
 import frc.robot.subsystems.Vision;
-
-import java.util.ArrayList;
 import java.util.logging.Logger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.sorutil.path.AsyncTrajectory;
 
 /** An example command that uses an example subsystem. */
-public class AutoAdjust extends CommandBase {
+public class MoveToScoringLocation extends CommandBase {
   private final Logger logger;
   private final Swerve swerve;
   private final DriveTrainPoseEstimator poseEstimator;
   private final Vision vision;
   private Pose2d currentPose;
   private Pose2d targetPose;
-  private int grid;
+  private final GridLocation selectedGridLocation;
+  
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public AutoAdjust(DriveTrainPoseEstimator poseEstimator, Swerve swerve, Vision vision) {
-    logger = Logger.getLogger(AutoAdjust.class.getName());
+  public MoveToScoringLocation(DriveTrainPoseEstimator poseEstimator, Swerve swerve, Vision vision, GridLocation selectedGridLocation) {
+    logger = Logger.getLogger(MoveToScoringLocation.class.getName());
     this.poseEstimator = poseEstimator;
     this.swerve = swerve;
     this.vision = vision;
+    this.selectedGridLocation = selectedGridLocation;
     currentPose = poseEstimator.getEstimatedPose();
     addRequirements(poseEstimator, vision, swerve);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if(grid == 1 || grid == 3)
-    switch(grid){
-      case 1:
-       AsyncTrajectory.generateTrajectory(currentPose, targetPose, new ArrayList<>(), Constants.Scoring.SCORING_TRAJECTORY_CONFIG);
-      case 2:
-
-    }
-    
-  } 
+  public void execute() {} 
 
   // Called once the command ends or is interrupted.
   @Override
