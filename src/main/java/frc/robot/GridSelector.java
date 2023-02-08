1ae115f8ad71789583d5389a5e28a92cbad86733
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2023-02-07 23:56:32 -0800

    Code cleanup

--- src/main/java/frc/robot/GridSelector.java
+++ src/main/java/frc/robot/GridSelector.java
@@ -1,53 +1,47 @@
 package frc.robot;
 
+import java.util.Arrays;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.subsystems.DriveTrainPoseEstimator;
 import frc.robot.subsystems.Vision;
 
 public class GridSelector {
-    private static int selectedGridId;
+    private static int SELECTED_GRID_ID;
     // gives closest apriltag to the bot
     public static int getClosestId(Vision vision, DriveTrainPoseEstimator poseEstimator) {
         Pose2d currentPose = poseEstimator.getEstimatedPose();
-
-        var allianceTargets =
-                Constants.Scoring.TARGETS_PER_ALLIANCE.get(DriverStation.getAlliance());
         double closestDistance = -1;
         int closestId = 0;
         // finds the closest april tag to the bot
-        for (int id : allianceTargets) {
-            double tempIdDistance = distanceFormula(currentPose.getX(), currentPose.getY(),
-                    getTagPose2d(id).getX(), getTagPose2d(id).getY());
+        for (int id : Constants.Scoring.TARGETS_PER_ALLIANCE.get(DriverStation.getAlliance())) {
+            double tempIdDistance = currentPose.getTranslation().getDistance(getTagPose2d(closestId).getTranslation());
             if (tempIdDistance > closestDistance) {
                 closestDistance = tempIdDistance;
                 closestId = id;
             }
         }
-        // checks if the closest april tag if within the minimum distance
+
+        // checks if the closest apriltag if within the minimum distance
         Pose2d targetTagPose = getTagPose2d(closestId);
-        if (distanceFormula(currentPose.getX(), currentPose.getY(), targetTagPose.getX(), targetTagPose.getY()) > Constants.Scoring.MIN_AUTOMOVE_DISTANCE) {
+        if (currentPose.getTranslation().getDistance(targetTagPose.getTranslation()) > Constants.Scoring.MIN_AUTOMOVE_DISTANCE) {
             return -1;
         }
-        selectedGridId = closestId;
+        SELECTED_GRID_ID = closestId;
         return closestId;
     }
     
     public static GridLocation getGridLocation(int id) {
-        switch (id) {
+        switch (id % 5) {
             case 1:
                 return GridLocation.RIGHT;
             case 2:
                 return GridLocation.MIDDLE;
             case 3:
-                return GridLocation.LEFT;
-            case 6:
-                return GridLocation.RIGHT;
-            case 7:
-                return GridLocation.MIDDLE;
-            case 8:
                 return GridLocation.LEFT;
             default:
                 return null;
@@ -55,7 +49,7 @@ public class GridSelector {
     }
 
     public static enum GridLocation {
-        LEFT, MIDDLE, RIGHT
+        RIGHT, MIDDLE, LEFT;
     }
 
     
@@ -64,11 +58,7 @@ public class GridSelector {
         return new Pose2d(temPose3d.getX(), temPose3d.getY(), new Rotation2d(temPose3d.getRotation().getX(), temPose3d.getRotation().getY()));
     }
 
-    private static double distanceFormula(double x1, double y1, double x2, double y2) {
-        return Math.sqrt((Math.pow(y2 - y1, 2)) + (Math.pow(x2 - x1, 2)));
-    }
-
     public static GridLocation getSelectedGrid() {
-        return getGridLocation(selectedGridId);
+        return getGridLocation(SELECTED_GRID_ID);
     }
 }
