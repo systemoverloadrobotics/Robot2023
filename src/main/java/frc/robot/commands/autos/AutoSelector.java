ed160b573ddaf052616f3c8a9f9cea2c6b14b471
OneLemonyBoi <onelemonyboii@gmail.com>
2023-10-06 19:16:24 -0700

    Fix AutoSelector

--- src/main/java/frc/robot/commands/autos/AutoSelector.java
+++ src/main/java/frc/robot/commands/autos/AutoSelector.java
@@ -4,11 +4,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.MoveStraightAuton;
+import frc.robot.commands.RotationControlledSwerveDrive;
 import frc.robot.commands.SlightlyMoreComplexBalance;
-import frc.robot.commands.autos.AutoPaths.PieceCount;
-import frc.robot.commands.autos.AutoPaths.ScoringPosition;
-import frc.robot.commands.autos.AutoPaths.StartingPosition;
 import frc.robot.subsystems.Swerve;
 
 public class AutoSelector {
@@ -17,8 +14,8 @@ public class AutoSelector {
 
     public AutoSelector(Swerve swerve) {
         autoSelector.setDefaultOption("GO_STRAIGHT", new SequentialCommandGroup(
-            new MoveStraightAuton(swerve, 0.5, -3, 0),
-            new MoveStraightAuton(swerve, 3, 3, 0)
+            new RotationControlledSwerveDrive(swerve, () -> 0.5, () -> -3, () -> 0).withTimeout(0.5),
+            new RotationControlledSwerveDrive(swerve, () -> 3, () -> 3, () -> 0).withTimeout(3)
         ));
         autoSelector.addOption("DO_NOTHING", null);
         // RED
